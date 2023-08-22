c83b3b465d4a18eb534e3a76943a2bea80ea2d78
Kai.ting.j <dear17530@gmail.com>
2023-08-22 23:53:04 +0800

    add gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/react-week2/' : '/',
+  plugins: [react()]
 })
